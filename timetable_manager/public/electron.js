74f50c2b51241c0bff5b6f8eaf6b682f1d0e91b2
Wimalaweera K.T <thushankavindu2266@gmail.com>
2021-03-25 10:52:45 +0530

    change electron ruining environment

--- timetable_manager/public/electron.js
+++ timetable_manager/public/electron.js
@@ -3,41 +3,32 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require("path");
 const isDev = require("electron-is-dev");
-const fs = require('fs');
 let mainWindow;
-
 function createWindow() {
     mainWindow = new BrowserWindow({
         width: 1200,
-        height: 680,
-        minWidth : 1000,
-        minHeight : 680,
-        frame: false,
-        webPreferences: {
-            nodeIntegration: true
-        }
+        height: 800,
+        icon: ""
     });
+
+
     mainWindow.loadURL(
-        isDev ?
-            "http://localhost:3000" :
-            `file://${path.join(__dirname, "../build/index.html")}`
+        isDev
+            ? "http://localhost:3000"
+            : `file://${path.join(__dirname, "../build/index.html")}`
     );
     mainWindow.on("closed", () => (mainWindow = null));
-    mainWindow.setMenu(null)
-
-
-
-
 }
+
 app.on("ready", createWindow);
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") {
         app.quit();
     }
 });
+
 app.on("activate", () => {
     if (mainWindow === null) {
         createWindow();
     }
 });
-
